9c89686ed151ad6ee7c021b6ff7d457ee9c052ae
Dragan Pilipovic <draganpilipovic1997bp@gmail.com>
2024-01-19 03:40:53 +0100

    add reverse

--- src/main/java/org/hyperledger/besu/ethereum/trie/verkle/hasher/PedersenHasher.java
+++ src/main/java/org/hyperledger/besu/ethereum/trie/verkle/hasher/PedersenHasher.java
@@ -41,8 +41,12 @@ public class PedersenHasher implements Hasher {
    */
   @Override
   public Bytes32 commit(Bytes32[] inputs) {
+    Bytes32[] rev = new Bytes32[inputs.length];
+    for (int i = 0; i < inputs.length; ++i) {
+      rev[i] = (Bytes32) inputs[i].reverse();
+    }
     Bytes input_serialized = Bytes.concatenate(inputs);
-    return Bytes32.wrap(LibIpaMultipoint.commit(input_serialized.toArray(), committerPointer));
+    return (Bytes32) Bytes32.wrap(LibIpaMultipoint.commit(input_serialized.toArray(), committerPointer)).reverse();
   }
 
   /**

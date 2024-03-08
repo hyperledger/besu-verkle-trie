ddd68d0b081c756c58adbd6c681b0812d1a8f44e
kevaundray <kevtheappdev@gmail.com>
2024-03-08 13:23:54 +0000

    chore: remove usage of `pedersenHash` in `trieKeyHash` (#43)
    Signed-off-by: Kevaundray Wedderburn <kevtheappdev@gmail.com>

--- src/main/java/org/hyperledger/besu/ethereum/trie/verkle/hasher/PedersenHasher.java
+++ src/main/java/org/hyperledger/besu/ethereum/trie/verkle/hasher/PedersenHasher.java
@@ -28,6 +28,19 @@ import org.apache.tuweni.bytes.Bytes32;
  * inputs using the pedersen commitment - multi scalar multiplication vector commitment algorithm.
  */
 public class PedersenHasher implements Hasher {
+
+  // The amount of bytes that we will take from the input when `trieKeyHash` is
+  // called.
+  private static final int CHUNK_SIZE = 16;
+
+  // The input will always be 64 bytes once it is padded in `trieKeyHash `
+  // and since we are taking 16 bytes at a time we will create four chunks from
+  // the input.
+  // An extra chunk which has a constant value is added as a domain separator and
+  // length encoder,
+  // making the total number of chunks equal to five.
+  private static final int NUM_CHUNKS = 5;
+
   /**
    * Commits an array of Bytes32 using the pedersen commitment - multi scalar multiplication vector
    * commitment algorithm.
@@ -68,8 +81,36 @@ public class PedersenHasher implements Hasher {
    */
   @Override
   public Bytes32 trieKeyHash(Bytes address, Bytes32 index) {
-    Bytes32 addr = Bytes32.leftPad(address);
-    Bytes input = Bytes.concatenate(addr, index);
-    return Bytes32.wrap(LibIpaMultipoint.pedersenHash(input.toArray()));
+
+    // Reverse the index so that it is in little endian format
+    final Bytes32 indexLE = Bytes32.wrap(index.reverse());
+
+    // Pad the address so that it is 32 bytes
+    final Bytes32 addr = Bytes32.leftPad(address);
+    final Bytes input = Bytes.concatenate(addr, indexLE);
+
+    final Bytes32[] chunks = new Bytes32[NUM_CHUNKS];
+
+    // Set the first chunk which is always 2 + 256 * 64
+    chunks[0] = Bytes32.rightPad(Bytes.of(2, 64));
+
+    // Given input is 64 bytes, we create exactly 4 chunks of 16 bytes each
+    // The chunks are then padded to 32 bytes since the commit methods requires 32
+    // byte scalars.
+    for (int i = 0; i < NUM_CHUNKS - 1; i++) {
+      // Slice input into 16 byte segments
+      Bytes chunk = input.slice(i * CHUNK_SIZE, CHUNK_SIZE);
+      // Pad each chunk to make it 32 bytes
+      chunks[i + 1] = Bytes32.rightPad(chunk);
+    }
+
+    final Bytes hashBE =
+        Bytes.wrap(LibIpaMultipoint.commitRoot(Bytes.concatenate(chunks).toArray()));
+
+    // commitRoot returns the hash in big endian format, so we reverse it to get it
+    // in little endian
+    // format. When we migrate to using `groupToField`, this reverse will not be
+    // needed.
+    return Bytes32.wrap(hashBE.reverse());
   }
 }

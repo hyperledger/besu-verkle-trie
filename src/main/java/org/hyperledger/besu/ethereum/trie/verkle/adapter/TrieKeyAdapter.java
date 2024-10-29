70f48daa67f5bfd18da64c7572520f1280ceb897
Karim Taam <karim.t2am@gmail.com>
2024-10-29 10:05:33 +0100

    renaming and refactoring of the code (#76)
    Signed-off-by: Karim Taam <karim.t2am@gmail.com>

--- src/main/java/org/hyperledger/besu/ethereum/trie/verkle/adapter/TrieKeyAdapter.java
+++ src/main/java/org/hyperledger/besu/ethereum/trie/verkle/adapter/TrieKeyAdapter.java
@@ -25,6 +25,7 @@ import static org.hyperledger.besu.ethereum.trie.verkle.util.Parameters.VERKLE_N
 import static org.hyperledger.besu.ethereum.trie.verkle.util.Parameters.VERKLE_NODE_WIDTH_LOG2;
 
 import org.hyperledger.besu.ethereum.trie.verkle.hasher.Hasher;
+import org.hyperledger.besu.ethereum.trie.verkle.hasher.PedersenHasher;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -45,12 +46,17 @@ public class TrieKeyAdapter {
 
   private final Hasher hasher;
 
+  /** Creates a TrieKeyAdapter with the default Perdersen hasher. */
+  public TrieKeyAdapter() {
+    this.hasher = new PedersenHasher();
+  }
+
   /**
    * Creates a TrieKeyAdapter with the provided hasher.
    *
    * @param hasher The hasher used for key generation.
    */
-  public TrieKeyAdapter(Hasher hasher) {
+  public TrieKeyAdapter(final Hasher hasher) {
     this.hasher = hasher;
   }
 
@@ -70,34 +76,37 @@ public class TrieKeyAdapter {
    * @param storageKey The storage key.
    * @return The generated storage key.
    */
-  public Bytes32 storageKey(Bytes address, Bytes32 storageKey) {
-    final UInt256 pos = locateStorageKeyOffset(storageKey);
-    final Bytes32 base = hasher.trieKeyHash(address, pos);
-    final UInt256 suffix = locateStorageKeySuffix(storageKey);
-    return swapLastByte(base, suffix);
+  public Bytes32 storageKey(final Bytes address, final Bytes32 storageKey) {
+    final Bytes stem = getStorageStem(address, storageKey);
+    final Bytes suffix = getStorageKeySuffix(storageKey);
+    return Bytes32.wrap(Bytes.concatenate(stem, suffix));
   }
 
-  public UInt256 locateStorageKeyOffset(Bytes32 storageKey) {
-    UInt256 index = UInt256.fromBytes(storageKey);
-    if (index.compareTo(HEADER_STORAGE_SIZE) < 0) {
-      return index.add(HEADER_STORAGE_OFFSET).divide(VERKLE_NODE_WIDTH);
+  public UInt256 getStorageKeyTrieIndex(final Bytes32 storageKey) {
+    final UInt256 uintStorageKey = UInt256.fromBytes(storageKey);
+    if (uintStorageKey.compareTo(HEADER_STORAGE_SIZE) < 0) {
+      return uintStorageKey.add(HEADER_STORAGE_OFFSET).divide(VERKLE_NODE_WIDTH);
     } else {
       // We divide by VerkleNodeWidthLog2 to make space and prevent any potential overflow
       // Then, we increment, a step that is safeguarded against overflow.
-      return index
+      return uintStorageKey
           .shiftRight(VERKLE_NODE_WIDTH_LOG2.intValue())
           .add(MAIN_STORAGE_OFFSET_SHIFT_LEFT_VERKLE_NODE_WIDTH);
     }
   }
 
-  public UInt256 locateStorageKeySuffix(Bytes32 storageKey) {
-    UInt256 index = UInt256.fromBytes(storageKey);
-    if (index.compareTo(HEADER_STORAGE_SIZE) < 0) {
-      final UInt256 mod = index.add(HEADER_STORAGE_OFFSET).mod(VERKLE_NODE_WIDTH);
-      return UInt256.fromBytes(mod.slice(mod.size() - 1));
+  public Bytes getStorageKeySuffix(final Bytes32 storageKey) {
+    final UInt256 uintStorageKey = UInt256.fromBytes(storageKey);
+    if (uintStorageKey.compareTo(HEADER_STORAGE_SIZE) < 0) {
+      return getLastByte(uintStorageKey.add(HEADER_STORAGE_OFFSET).mod(VERKLE_NODE_WIDTH));
     } else {
-      return UInt256.fromBytes(storageKey.slice(Bytes32.SIZE - 1));
+      return getLastByte(storageKey);
     }
+  }
+
+  public Bytes getStorageStem(final Bytes address, final Bytes32 storageKey) {
+    final UInt256 trieIndex = getStorageKeyTrieIndex(storageKey);
+    return hasher.computeStem(address, trieIndex);
   }
 
   /**
@@ -107,14 +116,23 @@ public class TrieKeyAdapter {
    * @param chunkId The chunk ID.
    * @return The generated code chunk key.
    */
-  public Bytes32 codeChunkKey(Bytes address, UInt256 chunkId) {
-    UInt256 pos = locateCodeChunkKeyOffset(chunkId);
-    Bytes32 base = hasher.trieKeyHash(address, pos.divide(VERKLE_NODE_WIDTH));
-    return swapLastByte(base, pos.mod(VERKLE_NODE_WIDTH));
+  public Bytes32 codeChunkKey(final Bytes address, final UInt256 chunkId) {
+    final Bytes stem = getCodeChunkStem(address, chunkId);
+    final Bytes suffix = getCodeChunkKeySuffix(chunkId);
+    return Bytes32.wrap(Bytes.concatenate(stem, suffix));
   }
 
-  public UInt256 locateCodeChunkKeyOffset(Bytes32 chunkId) {
-    return CODE_OFFSET.add(UInt256.fromBytes(chunkId));
+  public UInt256 getCodeChunkKeyTrieIndex(final Bytes32 chunkId) {
+    return CODE_OFFSET.add(UInt256.fromBytes(chunkId)).divide(VERKLE_NODE_WIDTH);
+  }
+
+  public Bytes getCodeChunkKeySuffix(final Bytes32 chunkId) {
+    return getLastByte(CODE_OFFSET.add(UInt256.fromBytes(chunkId)).mod(VERKLE_NODE_WIDTH));
+  }
+
+  public Bytes getCodeChunkStem(final Bytes address, final UInt256 chunkId) {
+    final UInt256 trieIndex = getCodeChunkKeyTrieIndex(chunkId);
+    return hasher.computeStem(address, trieIndex);
   }
 
   /**
@@ -124,21 +142,23 @@ public class TrieKeyAdapter {
    * @param leafKey The leaf key.
    * @return The generated header key.
    */
-  Bytes32 headerKey(Bytes address, UInt256 leafKey) {
-    Bytes32 base = hasher.trieKeyHash(address, UInt256.valueOf(0).toBytes());
-    return swapLastByte(base, leafKey);
+  public Bytes32 headerKey(final Bytes address, final UInt256 leafKey) {
+    final Bytes stem = getHeaderStem(address);
+    return Bytes32.wrap(Bytes.concatenate(stem, getLastByte(leafKey)));
+  }
+
+  public Bytes getHeaderStem(final Bytes address) {
+    return hasher.computeStem(address, UInt256.valueOf(0).toBytes());
   }
 
   /**
-   * Swaps the last byte of the base key with a given subIndex.
+   * Get the last byte of the base .
    *
    * @param base The base key.
-   * @param subIndex The subIndex.
-   * @return The modified key.
+   * @return The last byte of the key.
    */
-  public Bytes32 swapLastByte(Bytes32 base, Bytes subIndex) {
-    final Bytes lastByte = subIndex.slice(subIndex.size() - 1, 1);
-    return (Bytes32) Bytes.concatenate(base.slice(0, 31), lastByte);
+  public Bytes getLastByte(final Bytes32 base) {
+    return base.slice(Bytes32.SIZE - 1);
   }
 
   /**
@@ -161,7 +181,7 @@ public class TrieKeyAdapter {
     return headerKey(address, CODE_HASH_LEAF_KEY);
   }
 
-  public int getNbChunk(Bytes bytecode) {
+  public int getNbChunk(final Bytes bytecode) {
     return bytecode.isEmpty() ? 0 : (1 + ((bytecode.size() - 1) / CHUNK_SIZE));
   }
 
@@ -172,15 +192,15 @@ public class TrieKeyAdapter {
    * @param bytecode Code's bytecode
    * @return List of 32-bytes code chunks
    */
-  public List<UInt256> chunkifyCode(Bytes bytecode) {
+  public List<UInt256> chunkifyCode(final Bytes bytecode) {
     if (bytecode.isEmpty()) {
       return new ArrayList<>();
     }
 
     // Chunking variables
     final int CHUNK_SIZE = 31;
-    int nChunks = getNbChunk(bytecode);
-    int padSize = nChunks * CHUNK_SIZE - bytecode.size();
+    final int nChunks = getNbChunk(bytecode);
+    final int padSize = nChunks * CHUNK_SIZE - bytecode.size();
     final Bytes code = Bytes.concatenate(bytecode, Bytes.repeat((byte) 0, padSize));
     final List<UInt256> chunks = new ArrayList<>(nChunks);
 
